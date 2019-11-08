aefa69488b114a8bcc0c144c0bad63fe2fa2e4b0
bufftowel <basitalias11@gmail.com>
2019-11-09 01:50:09 +0530

    Improved Code Formatting

--- Big_Integer.cpp
+++ Big_Integer.cpp
@@ -3,7 +3,8 @@
 #include <iomanip>
 using namespace std;
 
-class BigInt {
+class BigInt
+{
 
     #ifndef ll
     typedef long long int ll;
@@ -31,14 +32,15 @@ class BigInt {
         }
         return ans;
     }
-    public:
+public:
     BigInt()
     {
         sign = 1;
     }
     void read(string &s)
     {
-        ll curr; int k = 0;
+        ll curr;
+        int k = 0;
         while(k<(int)s.size() && (s[k]=='-' || s[k]=='+'))
         {
             if(s[k]=='-')
@@ -52,7 +54,7 @@ class BigInt {
             {
                 curr = curr * 10 + s[j]-'0';
             }
-             a.push_back(curr);
+            a.push_back(curr);
         }
         removeZeros();
     }
@@ -62,14 +64,16 @@ class BigInt {
         stringstream ss;
         ss << *this;
         ss >> s;
-        if(!sign) s = '-' + s;
+        if(!sign)
+            s = '-' + s;
         return s;
     }
     void removeZeros()
     {
         while(!a.empty() && !a.back())
             a.pop_back();
-        if(a.empty()) sign = 1;
+        if(a.empty())
+            sign = 1;
     }
     BigInt operator + (const BigInt &b) const
     {
@@ -79,13 +83,18 @@ class BigInt {
         ll carry = 0;
         for(int i=0; i<(int)max(a.size(),b.a.size()); ++i)
         {
-            if(i==(int)ans.a.size()) ans.a.push_back(0);
+            if(i==(int)ans.a.size())
+                ans.a.push_back(0);
             carry = carry + ans.a[i];
-            if(i<(int)a.size()) {carry += a[i];}
+            if(i<(int)a.size())
+            {
+                carry += a[i];
+            }
             ans.a[i] = carry % p10;
             carry = carry / p10;
         }
-        if(carry) ans.a.push_back(carry);
+        if(carry)
+            ans.a.push_back(carry);
         return ans;
     }
     template <typename T>
@@ -111,32 +120,36 @@ class BigInt {
             ll carry = 0;
             for(int i=0; i< (int)ans.a.size(); ++i)
             {
-               if(i < (int)num.a.size())
-               {
-                   ans.a[i] = ans.a[i] - num.a[i] + carry;
-                   if(ans.a[i] < 0)
-                   {
-                       ans.a[i] += p10;
-                       carry = -1;
-                   }
-                   else carry = 0;
-               }
-               else if(carry)
-               {
-                   ans.a[i] = ans.a[i] + carry;
-                   if(ans.a[i] < 0)
-                   {
-                       ans.a[i] += p10;
-                       carry = -1;
-                   }
-                   else carry = 0;
-               }
-               else break;
+                if(i < (int)num.a.size())
+                {
+                    ans.a[i] = ans.a[i] - num.a[i] + carry;
+                    if(ans.a[i] < 0)
+                    {
+                        ans.a[i] += p10;
+                        carry = -1;
+                    }
+                    else
+                        carry = 0;
+                }
+                else if(carry)
+                {
+                    ans.a[i] = ans.a[i] + carry;
+                    if(ans.a[i] < 0)
+                    {
+                        ans.a[i] += p10;
+                        carry = -1;
+                    }
+                    else
+                        carry = 0;
+                }
+                else
+                    break;
             }
             ans.removeZeros();
             return ans;
         }
-        else return *this + (-num);
+        else
+            return *this + (-num);
     }
     template <typename T>
     BigInt operator - (const T &b) const
@@ -154,8 +167,10 @@ class BigInt {
         vector<ll> n(a.begin(),a.end()),m(num.a.begin(),num.a.end()),c;
         BigInt ans;
         ans.sign = !(sign ^ num.sign);
-        while(n.size() < m.size()) n.push_back(0);
-        while(n.size() > m.size()) m.push_back(0);
+        while(n.size() < m.size())
+            n.push_back(0);
+        while(n.size() > m.size())
+            m.push_back(0);
         c = karatsuba(n,m);
         ll carry = 0;
         for(int i = 0; i<(int)c.size(); ++i)
@@ -172,11 +187,11 @@ class BigInt {
         ans.removeZeros();
         return ans;
     }
-         // calling karatsuba will inevitably pass this (pointer to current object), and if katatsuba
-        // is not a const function, then this will cause error, as it may change it.
-       // if we declare karatsuba as static that would mean it won't receive
-      // a implicit object (They don't have access to this pointer). A Static member function can be
-     // called even without a object(using :: and class name). A static member function can only access static
+    // calling karatsuba will inevitably pass this (pointer to current object), and if katatsuba
+    // is not a const function, then this will cause error, as it may change it.
+    // if we declare karatsuba as static that would mean it won't receive
+    // a implicit object (They don't have access to this pointer). A Static member function can be
+    // called even without a object(using :: and class name). A static member function can only access static
     // static data member, other static member functions and any other functions from outside the class.
     bool operator > (const BigInt &num) const
     {
@@ -189,8 +204,9 @@ class BigInt {
             if(a[i] != num.a[i])
             {
                 if(sign == 1)
-                return a[i] > num.a[i];
-                else return a[i] < num.a[i];
+                    return a[i] > num.a[i];
+                else
+                    return a[i] < num.a[i];
             }
         }
         return 0;
@@ -202,7 +218,8 @@ class BigInt {
         if(a.size() != num.a.size())
             return 0;
         for(int i = a.size()-1; i > -1 ; --i)
-            if(a[i] != num.a[i]) return 0;
+            if(a[i] != num.a[i])
+                return 0;
         return 1;
     }
     bool operator < (const BigInt &num) const
@@ -241,7 +258,8 @@ class BigInt {
         return *this;
     }
     const BigInt operator ++ (int)   // postfix ( a dummy argument is required to distinguish b/w prefix and post fix C++)
-    {                               // const function to prevent assignment of postfix (as its not a lvalue in C++)
+    {
+        // const function to prevent assignment of postfix (as its not a lvalue in C++)
         BigInt x;
         x = *this;
         ++*this;
@@ -279,15 +297,17 @@ class BigInt {
     int length()
     {
         int l = a[a.size()-1], cnt = 0;
-        while(l) cnt++,l/=10;
+        while(l)
+            cnt++,l/=10;
         return 9*(a.size()-1)+cnt;
     }
-     // using reference of streams as they immediately need to be changed after use.
+    // using reference of streams as they immediately need to be changed after use.
     //  cannot define methods in stream classes (as left operand is the one operator's definition should be in)
-   //   hence overloading (<<,>>) as friend (not member function of this class but the scope is only in this class)
-  //    (you can also define them globally.)
+    //   hence overloading (<<,>>) as friend (not member function of this class but the scope is only in this class)
+    //    (you can also define them globally.)
     friend istream& operator >> (istream &stream, BigInt &a)  // return types are streams themselves so that chaining
-    {                                                        // is possible. e.g. cin>>a>>b;
+    {
+        // is possible. e.g. cin>>a>>b;
         string s;
         stream >> s;
         a.read(s);
@@ -295,10 +315,12 @@ class BigInt {
     }
     friend ostream& operator << (ostream &stream, BigInt num)
     {
-        if(!num.sign) stream << '-';
+        if(!num.sign)
+            stream << '-';
         if(num.a.empty())
             stream << '0';
-        else stream << num.a[num.a.size()-1];
+        else
+            stream << num.a[num.a.size()-1];
         for(int i=num.a.size()-2; i>-1; --i)
         {
             stream << setw(range) << setfill('0') << num.a[i];
@@ -323,7 +345,8 @@ class BigInt {
                     carry = ans[i+j]/p10;
                     ans[i+j] = ans[i+j]%p10;
                 }
-                if(carry) ans[i+n] += carry;
+                if(carry)
+                    ans[i+n] += carry;
             }
             return ans;
         }
@@ -366,19 +389,21 @@ class BigInt {
         if(carry)
             y2.push_back(carry);
         while(y2.size()>x2.size())
-                x2.push_back(0);
+            x2.push_back(0);
         while(y2.size()<x2.size())
-                y2.push_back(0);
+            y2.push_back(0);
         carry = 0;
         vector<ll> mid = karatsuba(x2,y2);
         k = mid.size();
         for(int i = 0; i<(int)ans.size(); ++i)
         {
-            if(i < (int)x2y2.size()) ans[i] += x2y2[i];
+            if(i < (int)x2y2.size())
+                ans[i] += x2y2[i];
             else
             {
                 ans[i] += carry;
-                if(!carry) break;
+                if(!carry)
+                    break;
             }
             carry = ans[i] / p10;
             ans[i] = ans[i] % p10;
@@ -386,15 +411,18 @@ class BigInt {
         carry = 0;
         for(int i = 0; i<(int)ans.size(); ++i)
         {
-            if(i<(int)x1y1.size()) ans[i] += x1y1[i];
+            if(i<(int)x1y1.size())
+                ans[i] += x1y1[i];
             else
             {
                 ans[i] += carry;
-                if(!carry) break;
+                if(!carry)
+                    break;
             }
             carry = ans[i+2*m] / p10;
             ans[i+2*m] = ans[i+2*m] % p10;
-        }carry = 0;
+        }
+        carry = 0;
         for(int i = 0; i<k; ++i)
         {
             mid[i] -= carry;
@@ -407,7 +435,8 @@ class BigInt {
                 mid[i] += 3ll*p10;
                 carry = 3;
             }
-        } carry = 0;
+        }
+        carry = 0;
         for(int i = 0; i<k; ++i)
         {
             ans[i + m] += mid[i];
@@ -416,10 +445,10 @@ class BigInt {
         }
         while(carry)
         {
-             ans[k]+=carry;
-             carry = ans[k]/p10;
-             ans[k] = ans[k] % p10;
-             k++;
+            ans[k]+=carry;
+            carry = ans[k]/p10;
+            ans[k] = ans[k] % p10;
+            k++;
         }
         return ans;
     }
